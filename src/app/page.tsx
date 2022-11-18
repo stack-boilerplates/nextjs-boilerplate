ba93ea628c8570679f2bc62197e0dd0565699e24
Vinicius Arcanjo <vinicius@arcanjo.dev>
2022-11-18 11:30:08 -0300

    feat: added prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 function Page() {
   return (
     <div>
       <h1>Page</h1>
-      <Link href="src/app/page#">
-        About
-      </Link>
+      <Link href="src/app/page#">About</Link>
     </div>
-  );
+  )
 }
 
-export default Page;
+export default Page

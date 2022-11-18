8441ead333a4d6e155d7f5fddd45755c94a32d08
Vinicius Arcanjo <vinicius@arcanjo.dev>
2022-11-18 12:50:20 -0300

    feat: added react test library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 export default function RootLayout({
-    children,
+  children
 }: {
-    children: React.ReactNode
+  children: React.ReactNode
 }) {
-    return (
-        <html title='Boilerplate'>
-        <head><title>Boilerplate</title></head>
-        <body>{children}</body>
-        </html>
-    )
+  return (
+    <html title="Boilerplate">
+      <head>
+        <title>Boilerplate</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
 }

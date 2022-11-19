4dbc0eb6d6deebe61c61d8928c1bc037fbfeb0d6
Vinicius Arcanjo <vinicius@arcanjo.dev>
2022-11-19 18:16:16 -0300

    feat: added PWA

--- next.config.js
+++ next.config.js
@@ -6,8 +6,16 @@ const nextConfig = {
     appDir: false
   },
   compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
     styledComponents: true
   }
 }
 
-module.exports = nextConfig
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true
+})
+
+module.exports = withPWA(nextConfig)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,11 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Boilerplate</title>
+        <link rel="shortcut icon" href="#" />
+        <link rel="apple-touch-icon" href="#" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
